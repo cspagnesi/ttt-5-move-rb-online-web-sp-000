796a40d1acc60159b21399b3818e7d42fc868c9c
Christopher Spagnesi <christopherspagnesi@gmail.com>
2020-11-09 20:33:44 +0000

    Automatically backed up by Learn

--- lib/move.rb
+++ lib/move.rb
@@ -6,7 +6,13 @@ def display_board(board)
   puts " #{board[6]} | #{board[7]} | #{board[8]} "
 end
 
+def input_to_index(user_input)
+user_input.to_i - 1
+end
 
+def move(location)
+location = X
 
+end
 
 
